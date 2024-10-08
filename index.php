1737e91b70e14a4d6b837597fef1b3c06b096766
Artur Kania <kania.artur13@gmail.com>
2024-10-08 11:54:17 +0200

    update between machines

--- index.php
+++ index.php
@@ -6,10 +6,41 @@ namespace App;
 
 require_once("src/Utils/debug.php");
 require_once("src/View.php");
+require_once("src/Controller.php");
+
+// error_reporting(0);
+// ini_set('display_errors', '0');
 
 const DEFAULT_ACTION = 'list';
 
 $action = $_GET['action'] ?? DEFAULT_ACTION;
 
 $view = new View();
-$view->render($action);
+$controller = new Controller();
+
+switch ($action) {
+    case 'create':
+        $page = 'create';
+        $created = false;
+
+        if (!empty($_POST)) {
+            $created = true;
+            $viewParams = [
+                "title" => $_POST["title"],
+                "description" => $_POST["description"]
+            ];
+        }
+        $viewParams['created'] = $created;
+        break;
+    case 'show':
+        $viewParams = [
+            "title" => 'Tytuł notatki',
+            "description" => 'Treść notatki'
+        ];
+        break;
+    default:
+        $page = 'list';
+        $viewParams['resultList'] = "wyświetlamy notatki";
+}
+
+$view->render($action, $viewParams);
